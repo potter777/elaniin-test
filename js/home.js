42ee6e62e1710b4e6a68ae976b6aeedd46516a20
René Cadenas <renmau91@gmail.com>
2018-05-26 21:25:06 -0600

    primera demo funcional

--- js/home.js
+++ js/home.js
@@ -1,4 +1,13 @@
 "use strict";
 let pageHome = new class {
+    constructor() {
+        this.sideNavElement = document.querySelector('.sidenav');
+        this.sideNav = M.Sidenav.init(this.sideNavElement, {});
+        let menuMobile = document.getElementById('nav-mobile');
+        menuMobile.addEventListener('click', function (event) {
+            event.preventDefault();
+            pageHome.sideNav.open();
+        });
+    }
 };
 //# sourceMappingURL=home.js.map
--- ts/home.ts
+++ ts/home.ts
@@ -1,40 +1,15 @@
 let pageHome = new class {
-    /*
-    infoPhoto: HTMLDivElement;
+
+    sideNavElement;
+    sideNav;
 
     constructor() {
-        this.prepareInfoPhotoElement();
-        //añadimos el evento onmouse a las imágenes de la galleria
-        let cards:Array<HTMLElement> = document.querySelectorAll('.works--card');
-        for (let card of cards) {
-            card.addEventListener('mouseover', this.onMouseOver_card);
-        }
+        this.sideNavElement = document.querySelector('.sidenav');
+        this.sideNav = M.Sidenav.init(this.sideNavElement, {});
+        let menuMobile:HTMLLinkElement = document.getElementById('nav-mobile');
+        menuMobile.addEventListener('click', function(event: Event) {
+            event.preventDefault();
+            pageHome.sideNav.open();
+        });
     }
-
-    onMouseOver_card(event: Event) {
-        console.log(this);
-    }
-
-    createInfoPhotoElement() {
-        this.infoPhoto = document.createElement('div');
-        let captionMake = document.createElement('p');
-        captionMake.className='caption';
-        captionMake.textContent = 'MAKE';
-        let captionModel = document.createElement('p');
-        captionModel.className='caption';
-        captionModel.textContent = 'MODEL';
-        let captionShutter = document.createElement('p');
-        captionShutter.className='caption';
-        captionShutter.textContent = 'SHUTTER SPEED';
-        let captionAperture = document.createElement('p');
-        captionAperture.className='caption';
-        captionAperture.textContent = 'APERTURE';
-        let captionFocal = document.createElement('p');
-        captionFocal.className='caption';
-        captionFocal.textContent = 'FOCAL LENGTH';
-        let captionISO = document.createElement('p');
-        captionISO.className='caption';
-        captionISO.textContent = 'ISO';
-    }
-    */
 }
