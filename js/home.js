0cf307cf20d16e97d1d2b23b6135be1317a3509d
René Cadenas <renmau91@gmail.com>
2018-05-26 20:31:05 -0600

    efectos a la galeria, añadidos

--- js/home.js
+++ js/home.js
@@ -1,2 +1,4 @@
 "use strict";
+let pageHome = new class {
+};
 //# sourceMappingURL=home.js.map
---
+++ ts/home.ts
@@ -0,0 +1,40 @@
+let pageHome = new class {
+    /*
+    infoPhoto: HTMLDivElement;
+
+    constructor() {
+        this.prepareInfoPhotoElement();
+        //añadimos el evento onmouse a las imágenes de la galleria
+        let cards:Array<HTMLElement> = document.querySelectorAll('.works--card');
+        for (let card of cards) {
+            card.addEventListener('mouseover', this.onMouseOver_card);
+        }
+    }
+
+    onMouseOver_card(event: Event) {
+        console.log(this);
+    }
+
+    createInfoPhotoElement() {
+        this.infoPhoto = document.createElement('div');
+        let captionMake = document.createElement('p');
+        captionMake.className='caption';
+        captionMake.textContent = 'MAKE';
+        let captionModel = document.createElement('p');
+        captionModel.className='caption';
+        captionModel.textContent = 'MODEL';
+        let captionShutter = document.createElement('p');
+        captionShutter.className='caption';
+        captionShutter.textContent = 'SHUTTER SPEED';
+        let captionAperture = document.createElement('p');
+        captionAperture.className='caption';
+        captionAperture.textContent = 'APERTURE';
+        let captionFocal = document.createElement('p');
+        captionFocal.className='caption';
+        captionFocal.textContent = 'FOCAL LENGTH';
+        let captionISO = document.createElement('p');
+        captionISO.className='caption';
+        captionISO.textContent = 'ISO';
+    }
+    */
+}
